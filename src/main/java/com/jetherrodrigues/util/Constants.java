2cff14d3c4beabd4ba7a827f25b438a06a864bd1
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2017-11-22 23:31:27 -0200

    change api token

--- src/main/java/com/jetherrodrigues/util/Constants.java
+++ src/main/java/com/jetherrodrigues/util/Constants.java
@@ -2,5 +2,5 @@ package com.jetherrodrigues.util;
 
 public final class Constants {
 	public static final String CLIMATEMPO_URL = "http://apiadvisor.climatempo.com.br/api/v1/";
-	public static final String TOKEN = "334ea9d9b10399afc14c74bd63134a2e";
+	public static final String TOKEN = "690989ff4d1ec93dc4b9976d0a424e6a";
 }
